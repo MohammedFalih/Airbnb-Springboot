edfcb59c7ef6eb5475089595a4f82e73536a6647
MohammedFalih <yunusfalih123@gmail.com>
2025-02-05 22:53:08 +0530

    feat: ui to display landlord property and delete

--- frontend-Angular/src/app/app.routes.ts
+++ frontend-Angular/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PropertiesComponent } from './landlord/properties/properties.component';
+import { authorityRouteAccess } from './core/auth/authority-route-access';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'landlord/properties',
+    component: PropertiesComponent,
+    canActivate: [authorityRouteAccess],
+    data: {
+      authorities: ['ROLE_LANDLORD'],
+    },
+  },
+//   {
+//     path: '',
+//     component: HomeComponent,
+//   },
+];
--- frontend-Angular/src/app/landlord/landlord-listing.service.ts
+++ frontend-Angular/src/app/landlord/landlord-listing.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import {
   computed,
   inject,
@@ -7,7 +7,7 @@ import {
   WritableSignal,
 } from '@angular/core';
 import { State } from '../core/model/state.model';
-import { CreatedListing, NewListing } from './model/listing.model';
+import { CardListing, CreatedListing, NewListing } from './model/listing.model';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -23,8 +23,17 @@ export class LandlordListingService {
   );
   createSig = computed(() => this.create$());
 
+  private getAll$: WritableSignal<State<Array<CardListing>>> = signal(
+    State.Builder<Array<CardListing>>().forInit()
+  );
+  getAllSig = computed(() => this.getAll$());
+
+  private delete$: WritableSignal<State<string>> = signal(
+    State.Builder<string>().forInit()
+  );
+  deleteSig = computed(() => this.delete$());
+
   create(newListing: NewListing): void {
-    console.log("createlisting:2")
     const formData = new FormData();
     for (let i = 0; i < newListing.pictures.length; ++i) {
       formData.append('picture-' + i, newListing.pictures[i].file);
@@ -40,11 +49,36 @@ export class LandlordListingService {
       .subscribe({
         next: (listing) =>
           this.create$.set(State.Builder<CreatedListing>().forSuccess(listing)),
-        error: (err) =>{
-          console.log("error on creating list: ", err);
+        error: (err) => {
+          console.log('error on creating list: ', err);
           this.create$.set(State.Builder<CreatedListing>().forError(err));
-        }
+        },
       });
+  }
+
+  getAll() {
+    this.http
+      .get<Array<CardListing>>(
+        `${environment.API_URL}/landlord-listing/get-all`
+      )
+      .subscribe({
+        next: (listing) =>
+          this.getAll$.set(
+            State.Builder<Array<CardListing>>().forSuccess(listing)
+          ),
+        error: (err) =>
+          this.create$.set(State.Builder<CreatedListing>().forError(err)),
+      });
+  }
+
+  delete(publicId: string) {
+    const params = new HttpParams().set("publicId", publicId)
+    this.http.delete<string>(
+      `${environment.API_URL}/landlord-listing/delete`, {params}
+    ).subscribe({
+      next: publicId => this.delete$.set(State.Builder<string>().forSuccess(publicId)),
+      error: error => this.create$.set(State.Builder<CreatedListing>().forError(error)),
+    })
   }
 
   resetListingCreation() {
